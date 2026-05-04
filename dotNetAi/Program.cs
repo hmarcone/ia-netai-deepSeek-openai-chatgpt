bd1dcf1fcbad941931932015c9a7aba5b72fdf7d
hmarcone <hmarcone@gmail.com>
2026-05-04 16:21:43 -0300

    Adicionando documentação do scalar

--- dotNetAi/Program.cs
+++ dotNetAi/Program.cs
@@ -20,16 +20,50 @@ builder.Services.AddCors(options => options.AddDefaultPolicy(builder =>
 }));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddOpenApi(options =>
+{
+	options.AddDocumentTransformer((document, context, _) =>
+	{
+		document.Info = new()
+		{
+			Title = ".NET AI hmarcone API",
+			Version = "v1",
+			Description = """  
+               This API provides AI-based features such as chat, image generation,  
+               recipe creation and audio transcription.  
+               """,
+			Contact = new()
+			{
+				Name = "hmarcone Training",
+				Email = "hmarcone@gmail.com",
+				Url = new Uri("https://hmarcone.com.br/meus-cursos")
+			},
+			License = new()
+			{
+				Name = "Apache 2 License",
+				Url = new Uri("https://hmarcone.com.br/meus-cursos")
+			},
+			TermsOfService = new Uri("https://hmarcone.com.br/meus-cursos")
+		};
+		return Task.CompletedTask;
+	});
+});
 
 var app = builder.Build();
+
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 	app.MapOpenApi();
-	app.MapScalarApiReference();
+	app.MapScalarApiReference(options =>
+	{
+		options.Title = ".NET AI hmarcone API";
+		options.Theme = ScalarTheme.Default;
+		options.DefaultHttpClient = new(ScalarTarget.Http, ScalarClient.Http11);
+	});
 }
 
 app.UseHttpsRedirection();
