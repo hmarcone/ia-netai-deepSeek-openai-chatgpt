d4b0186fdbbf1dde9ec703376aea4daccf7c23f9
hmarcone <hmarcone@gmail.com>
2026-05-04 14:30:59 -0300

    Add project files.

---
+++ dotNetAi/Controllers/GenerativeAIController.cs
@@ -0,0 +1,39 @@
+﻿using dotNetAi.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotNetAi.Controllers
+{
+	[ApiController]
+	public class GenerativeAIController : ControllerBase
+	{
+		private readonly ChatService _chatService;
+
+		public GenerativeAIController(ChatService chatService)
+		{
+			_chatService = chatService;
+		}
+
+		[HttpGet("ask-ai")]
+		public async Task<IActionResult> AskAi([FromQuery] string prompt)
+		{
+			if (string.IsNullOrWhiteSpace(prompt))
+			{
+				return BadRequest("The 'prompt' parameter is required and cannot be empty.");
+			}
+			var response = await _chatService.GetResponseAsync(prompt);
+			return Ok(response);
+		}
+
+		[HttpGet("ask-ai-options")]
+		public async Task<IActionResult> AskAiWithOptions([FromQuery] string prompt)
+		{
+			if (string.IsNullOrWhiteSpace(prompt))
+			{
+				return BadRequest("The 'prompt' parameter is required and cannot be empty.");
+			}
+			var response = await _chatService.GetResponseWithOptionsAsync(prompt);
+			return Ok(response);
+		}
+
+	}
+}
---
+++ dotNetAi/Extensions/OpenAIExtensions.cs
@@ -0,0 +1,29 @@
+﻿using OpenAI;
+
+namespace DotNetAiErudio.Extensions
+{
+    public static class OpenAIExtensions
+    {
+        public static WebApplicationBuilder AddOpenAI(this WebApplicationBuilder builder)
+        {
+            //var apiKey = builder.Configuration["OpenAI:Key"];
+            var apiKey = Environment.GetEnvironmentVariable("OPEN_AI_API_KEY");
+
+			// Tenta buscar especificamente nas variáveis do Usuário
+			var key = Environment.GetEnvironmentVariable("OPEN_AI_API_KEY", EnvironmentVariableTarget.User);
+
+			// Ou nas variáveis da Máquina (exige admin para criar, mas o código lê normal)
+			var keyMachine = Environment.GetEnvironmentVariable("OPEN_AI_API_KEY", EnvironmentVariableTarget.Machine);
+
+			if (string.IsNullOrEmpty(apiKey))
+            {
+                throw new InvalidOperationException("OpenAI API key is not set.");
+            }
+
+            var openAIClient = new OpenAIClient(apiKey);
+
+            builder.Services.AddSingleton(openAIClient);
+            return builder;
+        }
+    }
+}
